752315883c6c1b3159ddf16bb6855b9779c57217
pvskp <pvincius14@gmail.com>
2021-08-23 20:50:23 -0300

    Documenta o código

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 
-function applyCot(dolar, euro, bitcoin){
-    var actualDolar = document.getElementById("usd");
+function applyCot(dollar, euro, bitcoin) {
+    /*
+    Apply the values ​​received by the API to the HTML.
+
+    Receives: the dollar, euro and bitcoin avaluated in reais.
+    Returns: nothing.
+    */
+    var actualDollar = document.getElementById("usd");
     var actualEuro = document.getElementById("eur");
     var actualBitcoin = document.getElementById("btc");
 
-    actualDolar.innerText = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'brl'}).format(dolar);
-    actualEuro.innerText = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'brl'}).format(euro);
-    actualBitcoin.innerText = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'brl'}).format(bitcoin);
+    actualDollar.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'brl' }).format(dollar);
+    actualEuro.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'brl' }).format(euro);
+    actualBitcoin.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'brl' }).format(bitcoin);
 }
 
 function requestAPI(url) {
+    /*
+    Requests the API that returns conversions to BRL currency.
+
+    Receives: the API's URL.
+    Returns: a dictionary that contains information about USD-BRL, EUR-BRL and BTC-BRL conversions.
+    */
     var request = new XMLHttpRequest();
     request.open("GET", url, false);
     request.send();
@@ -21,13 +33,15 @@ function main() {
         "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
     );
 
-    var dolar = coinData["USDBRL"]["bid"],
+    var dollar = coinData["USDBRL"]["bid"],
         euro = coinData["EURBRL"]["bid"],
         bitcoin = coinData["BTCBRL"]["bid"];
 
-    applyCot(dolar, euro, bitcoin);
+    applyCot(dollar, euro, bitcoin);
 }
 
 main();
 
-setInterval(main, 10000)
+
+// updates every 1 second
+setInterval(main, 1000)
