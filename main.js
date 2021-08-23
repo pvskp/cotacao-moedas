c9e8cbd8edf072b18e853bc7261ff5bbc1fcfbac
Paulo Vinícius Pinto <p242863@dac.unicamp.br>
2021-08-23 10:53:26 -0300

    Adiciona textos

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow } = require(Electron)
+const { app, BrowserWindow } = require('electron')
 
 function createWindow(){
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        autoHideMenuBar: true
     })
     window.loadFile('index.html');
 }
